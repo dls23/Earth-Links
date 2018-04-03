3d59e169ea045e9421855f8759b5beb990c127d9
Dena Shaw <dena.l.shaw@gmail.com>
2018-04-03 15:29:41 -0500

    Initial setup

---
+++ src/main/java/com/example/Earth/Links/EarthLinksApplication.java
@@ -0,0 +1,12 @@
+package com.example.Earth.Links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthLinksApplication.class, args);
+	}
+}
